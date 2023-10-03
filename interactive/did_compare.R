505b8fb286cadde942d63c143a2d7efb98cc4602
TsaiLintung <101155567+TsaiLintung@users.noreply.github.com>
2023-10-04 07:57:08 +0800

    10 percent ???

--- interactive/did_compare.R
+++ interactive/did_compare.R
@@ -1,55 +1,72 @@
+
+rm(list = ls())
+gc()
+
+library(profvis)
+library(devtools)
+library(peakRAM)
+library(microbenchmark)
+library(DRDID)
+
+library(fastglm)
+library(data.table)
+library(collapse)
+library(speedglm)
+library(RcppArmadillo)
+library(BMisc)
+
+
+#set the wd to the source folder
+setwd("~/GitHub/EventStudyCode")
+
+load_all()
+load_all("~/GitHub/did")
+#library(did)
+
+
+# load event code ---------------------------------------------------------------------
+
+# simple ---------------------------------------------------------------------
+
+
+
+
 boot <- TRUE
-clust <- "G"
+clust <- "strat"
 
-did_result <- att_gt(yname = "y",
-                     gname = "G",
-                     idname = "unit",
-                     tname = "time",
-                     data = dt,
-                     #xformla = ~x,
-                     base_period = "universal",
-                     control_group = "notyettreated",
-                     est_method = "ipw",
-                     clustervars = clust,
-                     bstrap = boot,
-                     biters = 10000)
+ratio <- c()
+for(i in 1:5){
+  ep <- i*0.1
+  simdt <- sim_did(1e+02, 10, cov = "int", hetero = "dynamic", balanced = TRUE, second_outcome = FALSE, seed = i, stratify = TRUE,
+                   epsilon_size = ep)
+  dt <- simdt$dt
+  
+  dt[, y := y*100]
+  dt[, strat := s*G*x]
+  
+  result <- fastdid(dt, timevar = "time", cohortvar = "G", unitvar = "unit", result_type = "group_time", boot = boot,
+                   clustervar = clust
+  )
+  
+  
+  did_result <- att_gt(yname = "y",
+                       gname = "G",
+                       idname = "unit",
+                       tname = "time",
+                       data = dt,
+                       #xformla = ~x,
+                       base_period = "universal",
+                       control_group = "notyettreated",
+                       est_method = "ipw",
+                       clustervars = clust,
+                       bstrap = boot,
+                       biters = 10000,
+                       cband = FALSE)
+  
+  did_result_dt <- data.table(cohort = did_result$group, time = did_result$t, did_att = did_result$att, did_se = did_result$se)
+  compare <- did_result_dt |> merge(result, by = c("cohort", "time"), all = TRUE) 
+  ratio <- c(ratio, compare[,sum(did_se, na.rm = TRUE)/sum(se, na.rm = TRUE)])
+}
 
-did_result2 <- att_gt(yname = "y",
-                      gname = "G",
-                      idname = "unit",
-                      tname = "time",
-                      data = dt,
-                      #xformla = ~x,
-                      base_period = "universal",
-                      control_group = "notyettreated",
-                      est_method = "ipw",
-                      clustervars = clust,
-                      bstrap = boot,
-                      biters = 10000)
+mean(ratio)
 
-
-did_result_dt <- data.table(cohort = did_result$group, time = did_result$t, did_att = did_result$att, did_se = did_result$se, did_se2 = did_result2$se)
-
-setorder(did_result_dt, time, cohort)
-
-
-compare <- did_result_dt |> merge(result, by = c("cohort", "time"), all = TRUE) 
-compare[, ratio := did_se/se]
-compare[, ratio2 := did_se/did_se2]
-
-# 
-# compare <- compare[!is.na(did_se)]
-# compare[, se_diff := se-did_se]
-# compare[, se_rand := did_se2-did_se]
-# compare[, att_diff := att-did_att]
-# compare[, se_diff_ratio := abs(se_diff/did_se)]
-# compare[, att_diff_ratio := abs(att_diff/did_att)]
-# compare[, se_rand_ratio := abs(se_rand/did_se)]
-# View(compare)
-# 
-# 
-# timetaken(started.at)
-# result
-
-compare[, mean(ratio, na.rm = TRUE)]
-compare[, mean(ratio2, na.rm = TRUE)]
